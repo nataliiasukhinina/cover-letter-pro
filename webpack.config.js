e74550128a524eafa4eadbe85f8bfd4b9c787eec
Nataliia Sukhinina <nataliia.sukhinina@gmail.com>
2023-05-15 10:10:05 -0500

    Moved global anf process to polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
     new NodePolyfillPlugin(),
     new Dotenv({
       path: '.env'
