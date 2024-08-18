40dd894203fa189da6dfbfee10038be6f2b7ad06
Natasha Sukhinina <natasha.sukhinina@veeam.com>
2024-08-18 12:11:28 -0500

    OpenAI API key removed from env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,5 @@ import awsConfig from '../aws-exports';
 export const environment = {
   production: true,
   amplify: awsConfig,
-  openAiApiKey: '',
-  openAiOrganization: 'org-KOOCC1Ru8jtVOg1g6ycSKGpf'
+  openAiOrganization: 'org-KOOCC1Ru8jtVOg1g6ycSKGpf',
 };
