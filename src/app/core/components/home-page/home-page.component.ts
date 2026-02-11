075efeb00bbcdfe3cf4f541c6f4454ac9a0c2219
wuweillove <125422883+wuweillove@users.noreply.github.com>
2026-02-11 14:58:50 -0600

    Update HomePageComponent to use dynamic profile data

--- src/app/core/components/home-page/home-page.component.ts
+++ src/app/core/components/home-page/home-page.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProfileService } from '../../services/profile.service';
+import { UserProfile } from '../../models/User';
 
 @Component({
-  selector: 'demo-home-page',
-  templateUrl: './home-page.component.html'
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit, OnDestroy {
+  userProfile: UserProfile | null = null;
+  private subscription: Subscription = new Subscription();
 
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.profileService.getProfile().subscribe(profile => {
+        this.userProfile = profile;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
